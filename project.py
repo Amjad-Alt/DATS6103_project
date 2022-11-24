dfca98c307efd93a5a9a498692d713f35c8cbd17
kismat91 <99793019+kismat91@users.noreply.github.com>
2022-11-23 23:40:49 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,8 @@
+#%%[markdown]
+import numpy as np
+import pandas as pd
+import os
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+# %%
