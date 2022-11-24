49ac941cb2aa8022564c9077cb5a5d7bf67acf32
kismat khatri <kismatkhatri91@gmail.com>
2022-11-24 00:10:28 -0500

    Update project.py

--- project.py
+++ project.py
@@ -6,3 +6,13 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 # %%
+data = pd.read_csv("restaurant-1-orders.csv")
+
+# %%
+data.shape
+data.info()
+data.describe()
+# %%
+data["Total Price"] = data["Product Price"] * data["Quantity"]
+data
+# %%
