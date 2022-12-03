806bfe36bb28efbe0696e0b357f51de1f3389c7e
narayanamurthydontukurti <narayanamurthy@gwu.edu>
2022-12-03 01:25:55 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-#%%[markdown]
+# %%[markdown]
+import datetime
 import numpy as np
 import pandas as pd
 import os
@@ -11,7 +12,7 @@ import seaborn as sns
 # %%
 data = pd.read_csv("restaurant-1-orders.csv",  parse_dates=['Order Date'])
 
-#%%
+# %%
 data.isna().mean()
 # %%
 data.shape
@@ -24,7 +25,7 @@ data
 item_freq = data.groupby('Item Name').agg({'Quantity': 'sum'})
 item_freq = item_freq.sort_values(by=['Quantity'])
 top_20 = item_freq.tail(20)
-top_20.plot(kind="barh", figsize=(16,8))
+top_20.plot(kind="barh", figsize=(16, 8))
 plt.title('Top 20 sold items')
 # %%
 print('Number of unique item name: ', len(data['Item Name'].unique()))
@@ -33,35 +34,40 @@ print('Number of unique item name: ', len(data['Item Name'].unique()))
 item_freq = data.groupby('Item Name').agg({'Quantity': 'sum'})
 item_freq = item_freq.sort_values(by=['Quantity'])
 top_20 = item_freq.head(20)
-top_20.plot(kind="barh", figsize=(16,8))
+top_20.plot(kind="barh", figsize=(16, 8))
 plt.title('Least 20 sold items')
 # %%
-data1 = data ["Total Price"].mean()
+data1 = data["Total Price"].mean()
 print(data1)
 
-#%%
-data = data.dropna()
-data = data.loc[data['Order Date'] >='2016-08-01']
 # %%
-#Investigating average order volume by periods
-print("Daily:\n", data.groupby([pd.Grouper(key='Order Date', freq='D')])['Quantity'].sum().mean())
-print("Weekly:\n", data.groupby([pd.Grouper(key='Order Date', freq='W-MON')])['Quantity'].sum().mean())
-print("Monthly:\n", data.groupby([pd.Grouper(key='Order Date', freq='M')])['Quantity'].sum().mean())
+data = data.dropna()
+data = data.loc[data['Order Date'] >= '2016-08-01']
+# %%
+# Investigating average order volume by periods
+print("Daily:\n", data.groupby(
+    [pd.Grouper(key='Order Date', freq='D')])['Quantity'].sum().mean())
+print("Weekly:\n", data.groupby(
+    [pd.Grouper(key='Order Date', freq='W-MON')])['Quantity'].sum().mean())
+print("Monthly:\n", data.groupby(
+    [pd.Grouper(key='Order Date', freq='M')])['Quantity'].sum().mean())
 
 # %%
-#create relevant Database df1 for total and df2 for bombay aloo
+# create relevant Database df1 for total and df2 for bombay aloo
 df = data[['Order Date', 'Quantity']]
 df2 = data[data['Item Name'] == 'Bombay Aloo']
 df2 = df2[['Order Date', 'Quantity']]
-df = df.groupby([pd.Grouper(key='Order Date', freq='W-MON')])['Quantity'].sum().reset_index().sort_values('Order Date')
-df2 = df2.groupby([pd.Grouper(key='Order Date', freq='W-MON')])['Quantity'].sum().reset_index().sort_values('Order Date')
-#Add Seasonality features
+df = df.groupby([pd.Grouper(key='Order Date', freq='W-MON')]
+                )['Quantity'].sum().reset_index().sort_values('Order Date')
+df2 = df2.groupby([pd.Grouper(key='Order Date', freq='W-MON')]
+                  )['Quantity'].sum().reset_index().sort_values('Order Date')
+# Add Seasonality features
 df['Week'] = df['Order Date'].dt.isocalendar().week
 df['Month'] = df['Order Date'].dt.month
 df2['Week'] = df2['Order Date'].dt.isocalendar().week
 df2['Month'] = df2['Order Date'].dt.month
-#Add past volume features
-for i in range (1,15):
+# Add past volume features
+for i in range(1, 15):
     label = "Quantity_" + str(i)
     df[label] = df['Quantity'].shift(i)
     df2[label] = df2['Quantity'].shift(i)
@@ -71,51 +77,62 @@ for i in range (1,15):
 df = df.dropna()
 df2 = df2.dropna()
 print(df)
-#%%
+# %%
 print(df2)
 # %%
-## Orders by time of day
-# We are going to calculate the average number of orders that are placed in each hour 
+# Orders by time of day
+# We are going to calculate the average number of orders that are placed in each hour
 # of the day to give us an idea of ​​when the demand is greatest.
-data['hour'] = data['Order Date'].dt.hour # Add column with the time of the order
+# Add column with the time of the order
+data['hour'] = data['Order Date'].dt.hour
 data.sample(5)
-#%%
-data['date'] = data['Order Date'].dt.strftime('%y/%m/%d') # Add column with date
+# %%
+data['date'] = data['Order Date'].dt.strftime(
+    '%y/%m/%d')  # Add column with date
 data.sample(5)
 
 # The way we will calculate the average orders per hour is as follows:
-#For a specified hour, we will calculate the number of orders that were taken at that hour considering 
+# For a specified hour, we will calculate the number of orders that were taken at that hour considering
 # the average per day.
-#%%
+# %%
+
+
 def avg_hour(hour):
     by_hour = data[data['hour'] == hour]
     avg = len(by_hour['Order Number'].unique()) / len(data['date'].unique())
     return avg
 
+
 hours = pd.DataFrame(sorted(data['hour'].unique()))
-hours.rename(columns={0:'hour'}, inplace=True)
+hours.rename(columns={0: 'hour'}, inplace=True)
 hours['Average orders'] = hours['hour'].apply(avg_hour)
 hours.set_index('hour', inplace=True)
 hours.head()
-#%%
-hours.plot.bar(figsize=(11,6), rot=0)
+# %%
+hours.plot.bar(figsize=(11, 6), rot=0)
 plt.xlabel('Hour')
 plt.title('Average number of orders by hour of the day')
-# As can be seen, the hours at which the greatest number of orders are made on average 
+# As can be seen, the hours at which the greatest number of orders are made on average
 # are 5, 6, and 7:00 p.m., with a peak at 6:00 p.m.
-#%%
-## Orders by day of the week
-# We are going to do the same analysis as before but this time considering the 
+# %%
+# Orders by day of the week
+# We are going to do the same analysis as before but this time considering the
 # different days of the week.
-data['day'] = data['Order Date'].dt.day_name() # Column with the name of the day
+# Column with the name of the day
+data['day'] = data['Order Date'].dt.day_name()
 data.sample(5)
-#%%
+# %%
+
+
 def by_day(day):
     data_day = data[data['day'] == day]
-    avg = len(data_day['Order Number'].unique()) / len(data_day['date'].unique())
+    avg = len(data_day['Order Number'].unique()) / \
+        len(data_day['date'].unique())
     return(avg)
 
-days = pd.DataFrame(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+
+days = pd.DataFrame(['Monday', 'Tuesday', 'Wednesday',
+                    'Thursday', 'Friday', 'Saturday', 'Sunday'])
 days.rename(columns={0: 'day'}, inplace=True)
 days['avg_orders'] = days['day'].apply(by_day)
 days
@@ -126,16 +143,15 @@ plt.xlabel('Day of week')
 plt.ylabel('Average number of orders')
 plt.title('Average orders by day of week')
 plt.xticks(rotation=90)
-# The graph shows that the day with the highest average number of orders is Saturday. 
+# The graph shows that the day with the highest average number of orders is Saturday.
 # Interestingly, more averages are performed on Friday than on Sundays.
 
-#%%
-#We will visualize sales over time considering monthly time periods
+# %%
+# We will visualize sales over time considering monthly time periods
 print('First Sale: ', data['Order Date'].min())
 print('Last Sale: ', data['Order Date'].max())
 # %%
-#We will then consider the dates between January 2016 and December 2019.
-import datetime
+# We will then consider the dates between January 2016 and December 2019.
 
 months = []
 
@@ -148,14 +164,17 @@ monthly = pd.DataFrame(months)
 monthly.rename(columns={0: 'month'}, inplace=True)
 monthly.head()
 
-#Now we will assign each month its total sales.
+# Now we will assign each month its total sales.
 
 # %%
+
+
 def sales_month(date):
     year_month = date.strftime('%y/%m')
     data1 = data[data['date'].str[:5] == year_month].copy()
     total = (data1['Quantity'] * data1['Product Price']).sum()
     return(total)
+
 
 monthly['total'] = monthly['month'].apply(sales_month)
 monthly.head()
@@ -164,13 +183,13 @@ plt.plot(monthly['month'], monthly['total'])
 plt.xlabel('Date')
 plt.ylabel('Total sales (USD)')
 plt.title('Total monthly sales')
-# You can see that monthly sales had been growing up to a point in the middle of 
+# You can see that monthly sales had been growing up to a point in the middle of
 # 2019, where they suffered a big drop. Let's identify this point.
 # %%
 monthly[monthly['month'] >= datetime.date(2019, 1, 1)]
-#The month in which sales fell was August 2019.
+# The month in which sales fell was August 2019.
 # %%
-## Order price distribution
+# Order price distribution
 
 # We are going to visualize the distribution of the cost of the orders to the restaurant.
 order_total = data[['Order Number', 'Quantity', 'Product Price']].copy()
@@ -182,17 +201,27 @@ plt.boxplot(order_totals['total'])
 plt.title('Order price distribution')
 
 
-#%%
+# %%
 p_95 = order_totals['total'].describe(percentiles=[0.95])['95%']
-print('95% of the orders are less than or equal to {percentile} USD'.format(percentile=p_95))
+print('95% of the orders are less than or equal to {percentile} USD'.format(
+    percentile=p_95))
 # 95% of the orders are less than or equal to 62.2 USD
 # Let's consider the distribution for the total price of orders less than 63 USD.
 
-#%%
+# %%
 plt.boxplot(order_totals[order_totals['total'] < 63]['total'])
 plt.title('Order total USD')
 plt.ylabel('USD')
 
-#%%
+# %%
 sns.distplot(order_totals[order_totals['total'] < 63], bins=20)
 plt.title('Order price distribution')
+
+# %%
+
+Min_quantity = data['Quantity'].min()
+Max_Quantity = data['Quantity'].max()
+print(Min_quantity)
+print(Max_Quantity)
+
+# %%
